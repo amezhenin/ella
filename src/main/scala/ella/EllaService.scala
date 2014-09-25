52a5551ff123d98935144c4c55a217251b7d7ab3
Artem Mezhenin <a.mezhenin@gmail.com>
2014-09-25 22:31:56 +0300

    Pool of workers

--- src/main/scala/ella/EllaService.scala
+++ src/main/scala/ella/EllaService.scala
@@ -1,7 +1,8 @@
 package ella
 
-import akka.actor.Actor
+import akka.actor.{Terminated, Props, Actor}
 import spray.routing._
+import akka.routing._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -18,12 +19,47 @@ class EllaActor extends Actor with EllaService {
 }
 
 
+/* Master worker which holds pool of workers*/
+class Master extends Actor {
+  var router = {
+    val routees = Vector.fill(100) {
+      val r = context.actorOf(Props[EllaActor])
+      context watch r
+      ActorRefRoutee(r)
+    }
+
+    /** Routing strategies have huge impact on throughput */
+    Router(RoundRobinRoutingLogic(), routees)
+    //Router(SmallestMailboxRoutingLogic(), routees)
+    //Router(RandomRoutingLogic(), routees) // worst results among others
+  }
+
+  def receive = {
+    case Terminated(a) =>
+      println("Terminated")
+      router = router.removeRoutee(a)
+      val r = context.actorOf(Props[EllaActor])
+      context watch r
+      router = router.addRoutee(r)
+
+    case w =>
+      router.route(w, sender())
+
+  }
+}
+
 // this trait defines our service behavior independently from the service actor
 trait EllaService extends HttpService {
 
   val route =
     path("") {
       get (ctx => ctx.complete("Hello world"))
+    } ~
+    path("sleep") {
+      get { ctx =>
+        Thread.sleep(1000)
+        ctx.complete("sleep")
+      }
     } ~
     pathPrefix(IntNumber) { num =>
       get (_.complete("number: " + num.toString))
